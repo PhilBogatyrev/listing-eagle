4d1414a8c1cb9ae96659374d431e8fe335a36b5f
Caiteoro <65575745+Caiteoro@users.noreply.github.com>
2022-12-05 18:34:45 -0500

    cats

--- components/ui/search/Search.js
+++ components/ui/search/Search.js
@@ -5,6 +5,10 @@ const Search = ({ query, querySent, onSearch }) => {
   console.log("query: ", query);
   const [message, setMessage] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const [fact, setFact] = useState({
+    items: [],
+    isLoaded: false,
+  });
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -17,20 +21,26 @@ const Search = ({ query, querySent, onSearch }) => {
     event.preventDefault();
     setSearchQuery(event.target.queryField.value);
     onSearch({ query: event.target.queryField.value, querySent: querySent });
+    newCatFact();
   };
 
-  async function getCatFacts() {
-    const res = await fetch("https://catfact.ninja/fact");
-    const posts = await res.json();
-
-    return posts;
-  }
+  const newCatFact = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((json) => {
+        setFact({
+          items: json,
+          isLoaded: true,
+        });
+        console.log(json.fact);
+      });
+  };
 
   return (
     <div className="topnav">
       <h1>query: {query}</h1>
       <h1>searchQuery: {searchQuery}</h1>
-      <h1>cat facts: {getCatFacts()}</h1>
+      <h1>cat fact: {fact.items.fact}</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
