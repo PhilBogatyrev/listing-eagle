2226b1045b08fcf7b0b8cea1ca0c6655119fda9f
Caiteoro <65575745+Caiteoro@users.noreply.github.com>
2022-12-12 22:06:35 -0500

    ebay

--- components/ui/search/Search.js
+++ components/ui/search/Search.js
@@ -25,7 +25,8 @@ const Search = ({ query, querySent, onSearch }) => {
     onSearch({ query: event.target.queryField.value, querySent: querySent });
     newCatFact();
     //ebayResponse();
-    walmartSearch();
+    //walmartSearch();
+    getToken();
   };
 
   const newCatFact = () => {
@@ -52,8 +53,9 @@ const Search = ({ query, querySent, onSearch }) => {
   const privateKey =
     "-----BEGIN PRIVATE KEY-----MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvBTvvOaSjaGCoxOUcjQG4xqWDnt/mq/2IV8m1cmaaCwUJQLrsOXK7Q46JziP4o9IWpzV9S1NAJPzaLOR6JsnyDNa8lWNDQDd6kL9m+6Qzn4bc6/FFAUYB6BFQi1HbguIGMfJOdqnR5tFYNoZzd5Fx2YSqWwF0LgoW1fH3e5Oe9ZVelzouDPTzF0ewRIRKaVe2nG2hwqqG0XWDnYt65TvQRFPwFtE38GhJgVrjokFL89cFE9ZfyMh5KYV6myEUFz5gQpqigVhG9z6mQl6pIeoACW17bIAOpQQRyX/bNLkQoQPo+Z7YQ/kHKwSTMBLj6BBB7IaaB3UIyCZMPA1bZw0dAgMBAAECggEBAJwJEpII1htJJ0PPpmN54xsVHuLldcB1fadcfNxjjc2wHWFOcFfCL+N8K0v4mGFHzDmeATYfPa1mTHdGpruwcUJ258thdAm06sE+4yWxG4dapiW3xHY82GlCuI0SaHxHh7L2PIxvhp84JVN76Qpwgx75SimG0Rj/IseRHhQB/G4uEuoU98ia0dsCQzD9iOB1kxwpDXdgMWLZNnceV4m1s0b9Rt4axnfvjTdVv9DIyipHsAuPqbSlX66zEeBPAsdYgiN10TdO0IaCfnalAdZ8M+GLUn6HbuCxA3jK2fQypd8eo1ZdLAIP2O08OzB+33gnQ33Ib4YgsqyZEHB+K+S5PwECgYEA17U30WpCYCX1NzzUevA7KmGXzv3RgZAANy1io5ReH2ZdF1NcfCFcahzf4PBcMZEEuVLO0FJReHqkATeauXhF/oZDYR4EbQlhQ+J/qtJO4W+sl9gpWwazBmyXWnEN2zgw2IvSupgi1U3GPgxxpXE5vE1NLvEo0yaAOlWYeTRdizECgYEAz7ZplWVwNQyy42CzB5dIQeZb8Z+WnkBTrr8Dej22owJXM4TeNIxgwoDzvxQhyZ0G3vCKTDmHspygrd7s1RUQdWkhd5tOdWiUv5wni0ldRFZAHAfsGusHMkR6ZKbtxp2wdgCGjgbWjdInEKzNC1M1yWi/vfJfQEJgVKfvvo1mja0CgYBHTFDc2UCDQ0igL20UDZ+gzI0/D8pWBuDrXAcEhp/txQn0Ecu6A9TmBhFB6nCuuX5UOzi8xtjC99aJyHVN5KFOFdbjnOZdnAIYTB9iuQz1mtHzhrF0suwtrWsBq+imkxYiHWVT48Fvu7tQ+w/9XUV2Jh7i6zdRQ6KSpYQ2W8anoQKBgHafwqTjjbV+Txu0Ogp3x3h7Sr8vWSF/s0r1I9NIM2ndCFgP5ac1FH2Aj7Psl29BEo936EOoB2tmMA2cZr06jm+/XrGkLKa411qmn57Ygz9n0q0viRPGbhGzMgMRonHv1LtPiYjLNvU/s7e+OEvjdbBU9kxQgz17qvAuCKlWr97xAoGBAMk5zMGtxI87t1sEqybVRowTHofWhZVJS2lOeUMgYh3Xv+TtdMtMhTXXz7dgXd7j5Vk1Vawdo/sqTgtiyQ8xlUxVW+B+tzEHhcq8mh98FcYIqf0SPOmFiJBbuGyC+qIrXNFiGPgIetdXk2arXOr87XAQ4QLmQvs/YPgQ3fv4vTVm-----END PRIVATE KEY-----";
 
+  const consumerID = "45954df0-cff0-4104-bddd-4cbdc353f0aa";
   const walmartAuthHeaders = generateWalmartAuthHeaders(
-    "45954df0-cff0-4104-bddd-4cbdc353f0aa",
+    consumerID,
     "1",
     privateKey
   );
@@ -64,6 +66,7 @@ const Search = ({ query, querySent, onSearch }) => {
       {
         method: "GET",
         headers: walmartAuthHeaders,
+        mode: "no-cors",
         "Content-Type": "application/json",
       }
     ).then((res) => console.log(res));
@@ -92,6 +95,7 @@ const Search = ({ query, querySent, onSearch }) => {
   const getToken = () => {
     const requestOptions = {
       method: "POST",
+      mode: "no-cors",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: base64ClientCredentials,
